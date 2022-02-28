a2a1531e6c4b9ba081d63e5e9903edf5540a7155
Seth Hoenig <seth.a.hoenig@gmail.com>
2022-02-28 16:24:01 -0600

    client: enable support for cgroups v2
    This PR introduces support for using Nomad on systems with cgroups v2 [1]
    enabled as the cgroups controller mounted on /sys/fs/cgroups. Newer Linux
    distros like Ubuntu 21.10 are shipping with cgroups v2 only, causing problems
    for Nomad users.
    
    Nomad mostly "just works" with cgroups v2 due to the indirection via libcontainer,
    but not so for managing cpuset cgroups. Before, Nomad has been making use of
    a feature in v1 where a PID could be a member of more than one cgroup. In v2
    this is no longer possible, and so the logic around computing cpuset values
    must be modified. When Nomad detects v2, it manages cpuset values in-process,
    rather than making use of cgroup heirarchy inheritence via shared/reserved
    parents.
    
    Nomad will only activate the v2 logic when it detects cgroups2 is mounted at
    /sys/fs/cgroups. This means on systems running in hybrid mode with cgroups2
    mounted at /sys/fs/cgroups/unified (as is typical) Nomad will continue to
    use the v1 logic, and should operate as before. Systems that do not support
    cgroups v2 are also not affected.
    
    When v2 is activated, Nomad will create a parent called nomad.slice (unless
    otherwise configured in Client conifg), and create cgroups for tasks using
    naming convention <allocID>-<task>.scope. These follow the naming convention
    set by systemd and also used by Docker when cgroups v2 is detected.
    
    Client nodes now export a new fingerprint attribute, unique.cgroups.version
    which will be set to "v1" or "v2" to indicate the cgroups regime in use by
    Nomad.
    
    The new cpuset management strategy fixes #11705, where docker tasks that
    spawned processes on startup would "leak". In cgroups v2, the PIDs are
    started in the cgroup they will always live in, and thus the cause of
    the leak is eliminated.
    
    [1] https://www.kernel.org/doc/html/latest/admin-guide/cgroup-v2.html
    
    Closes #11289
    Fixes #11705 #11773 #11933

--- plugins/drivers/testutils/testing.go
+++ plugins/drivers/testutils/testing.go
@@ -9,7 +9,8 @@ import (
 	"strings"
 	"time"
 
-	testing "github.com/mitchellh/go-testing-interface"
+	"github.com/hashicorp/nomad/client/lib/cgutil"
+	"github.com/opencontainers/runc/libcontainer/cgroups/fs2"
 
 	hclog "github.com/hashicorp/go-hclog"
 	plugin "github.com/hashicorp/go-plugin"
@@ -24,6 +25,7 @@ import (
 	"github.com/hashicorp/nomad/plugins/base"
 	"github.com/hashicorp/nomad/plugins/drivers"
 	"github.com/hashicorp/nomad/plugins/shared/hclspec"
+	testing "github.com/mitchellh/go-testing-interface"
 	"github.com/stretchr/testify/require"
 )
 
@@ -34,6 +36,7 @@ type DriverHarness struct {
 	t      testing.T
 	logger hclog.Logger
 	impl   drivers.DriverPlugin
+	cgroup string
 }
 
 func (h *DriverHarness) Impl() drivers.DriverPlugin {
@@ -53,12 +56,10 @@ func NewDriverHarness(t testing.T, d drivers.DriverPlugin) *DriverHarness {
 	)
 
 	raw, err := client.Dispense(base.PluginTypeDriver)
-	if err != nil {
-		t.Fatalf("err dispensing plugin: %v", err)
-	}
+	require.NoError(t, err, "failed to dispense plugin")
 
 	dClient := raw.(drivers.DriverPlugin)
-	h := &DriverHarness{
+	return &DriverHarness{
 		client:       client,
 		server:       server,
 		DriverPlugin: dClient,
@@ -66,13 +67,30 @@ func NewDriverHarness(t testing.T, d drivers.DriverPlugin) *DriverHarness {
 		t:            t,
 		impl:         d,
 	}
+}
 
-	return h
+// setCgroup creates a v2 cgroup for the task, as if a Client were initialized
+// and managing the cgroup as it normally would.
+//
+// Uses testing.slice as a parent.
+func (h *DriverHarness) setCgroup(allocID, task string) {
+	if cgutil.UseV2 {
+		h.cgroup = filepath.Join(cgutil.CgroupRoot, "testing.slice", cgutil.CgroupScope(allocID, task))
+		_ = fs2.CreateCgroupPath(h.cgroup, nil)
+	}
 }
 
 func (h *DriverHarness) Kill() {
-	h.client.Close()
+	_ = h.client.Close()
 	h.server.Stop()
+	h.cleanupCgroup()
+}
+
+func (h *DriverHarness) cleanupCgroup() {
+	if cgutil.UseV2 {
+		mgr, _ := fs2.NewManager(nil, h.cgroup, false)
+		_ = mgr.Destroy()
+	}
 }
 
 // tinyChroot is useful for testing, where we do not use anything other than
@@ -139,6 +157,7 @@ func (h *DriverHarness) MkAllocDir(t *drivers.TaskConfig, enableLogs bool) func(
 
 	// Create the mock allocation
 	alloc := mock.Alloc()
+	alloc.ID = t.AllocID
 	if t.Resources != nil {
 		alloc.AllocatedResources.Tasks[task.Name] = t.Resources.NomadResources
 	}
@@ -156,6 +175,9 @@ func (h *DriverHarness) MkAllocDir(t *drivers.TaskConfig, enableLogs bool) func(
 			}
 		}
 	}
+
+	// set cgroup
+	h.setCgroup(alloc.ID, task.Name)
 
 	//logmon
 	if enableLogs {
@@ -189,6 +211,7 @@ func (h *DriverHarness) MkAllocDir(t *drivers.TaskConfig, enableLogs bool) func(
 	return func() {
 		h.client.Close()
 		allocDir.Destroy()
+		h.cleanupCgroup()
 	}
 }
 
